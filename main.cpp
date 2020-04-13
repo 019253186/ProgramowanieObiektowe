72c7179668577a9cc68cf52e9e63dfa85d25b3fe
Piotr Szwed <63266143+019253186@users.noreply.github.com>
2020-04-13 14:30:59 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+# include "tablica.cpp"
+# include "tablica_wys.cpp"
+# include "menu.cpp"
+using namespace std;
+
+int main()
+{
+
+    cout<<"TWORZENIE TABLICY"<<"-------"<<"Wczytuje z pliku"<<endl;
+
+Tablice t1;
+t1.wczytaj_plik();
+
+
+
+
+
+
+
+
+    return 0;
+}
---
+++ naglowki.h
@@ -0,0 +1,8 @@
+#include <iostream>
+
+ using  namespace  std ;
+class Tablice{
+public:
+void wczytaj_plik();
+int tab[4][4];
+};
