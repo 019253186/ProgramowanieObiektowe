15b3040f6e3ba89bacd8a7c4b6a8cf8d80b1ef24
Piotr Szwed <63266143+019253186@users.noreply.github.com>
2020-05-05 01:39:26 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+
+
+	int n , m ;      //n=wiersze  m=kolumny
+
+	cout<<"podaj ilosc wierszy : "<<endl;
+	cin>>n;
+	cout<<"Podaj ilosc kolumn : "<<endl;
+	cin>>m;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
